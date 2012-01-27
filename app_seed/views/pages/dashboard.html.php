11f2d3d195cd641c4f9c1c78661b4eb7df72fd08
Dave Mingos <davemingos@me.com>
2012-01-26 23:28:57 -0500

    Moved all but the Sprout library ... out to the sprout-php-framework repository, which includes sprout and other libraries as submodules.

--- app_seed/views/pages/dashboard.html.php
+++
@@ -1,3 +0,0 @@
-<h1><?php echo $this->escape($greeting); ?></h1>
-
-<p>Today's date is <?php echo $this->escape($today); ?>.</p>
--- app_seed/webroot/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Copyright (c) <YEAR>, <YOU OR YOUR ORGANIZATION>
- * All rights reserved.
-*/
-
-require_once realpath('../config/application.php');
-
-\Sprout\Dispatcher::dispatch();
